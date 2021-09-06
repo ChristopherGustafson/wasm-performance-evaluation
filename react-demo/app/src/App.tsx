9c9f826dc14acdc47af10d15196ae28e2b0f2252
Ackuq@ <axel@pettersson.cc>
2021-09-06 17:49:40 +0200

    Create simple wasm application

--- react-demo/app/src/App.tsx
+++ react-demo/app/src/App.tsx
@@ -1,8 +1,32 @@
+import { ChangeEvent, useEffect, useState } from "react";
+
+const calculateResult = async (a: number, b: number) => {
+    const wasm = await import("wasm");
+    return wasm.add_two_ints(a, b);
+};
+
 const App = () => {
-    import("wasm").then((module) => {
-        console.log(module);
-    });
-    return <div className="App"></div>;
+    const [state, setState] = useState({ a: 0, b: 0 });
+    const [result, setResult] = useState(0);
+
+    const handleChange = (id: keyof typeof state) => (event: ChangeEvent<HTMLInputElement>) => {
+        setState((prev) => ({ ...prev, [id]: event.target.value }));
+    };
+
+    useEffect(() => {
+        calculateResult(state.a, state.b).then(setResult);
+    }, [state]);
+
+    return (
+        <div className="App">
+            <input type="number" value={state.a} onChange={handleChange("a")} />
+            <input type="number" value={state.b} onChange={handleChange("b")} />
+
+            <div>
+                {state.a} + {state.b} = {result}
+            </div>
+        </div>
+    );
 };
 
 export default App;
